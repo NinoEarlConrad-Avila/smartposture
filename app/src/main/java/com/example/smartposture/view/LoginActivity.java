d016653ce16f1e8ac7b801aa6b83235ac0b6c286
fronald143 <120772118+fronald143@users.noreply.github.com>
2024-04-25 08:04:20 +0800

    clickable sign up

--- app/src/main/java/com/example/smartposture/view/LoginActivity.java
+++ app/src/main/java/com/example/smartposture/view/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.smartposture.Home;
@@ -20,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private Button loginButton;
     private LoginViewModel loginVM;
-
+    private TextView signUpTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +32,14 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.btnLogin);
         loginVM = new ViewModelProvider(this).get(LoginViewModel.class);
 
+        signUpTextView = findViewById(R.id.txtSignUp);
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
