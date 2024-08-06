433378ce96c8b80b8c20a8058e43407c7da90c11
earlBA <earl01000010@gmail.com>
2024-08-07 01:56:47 +0800

    Able to Detect Posture and Count Repetitions

---
+++ app/src/main/java/com/example/smartposture/posedetector/classification/PoseClassifierProcessor.java
@@ -0,0 +1,144 @@
+package com.example.smartposture.posedetector.classification;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.WorkerThread;
+
+import com.google.mlkit.vision.pose.Pose;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Accepts a stream of {@link Pose} for classification and rep counting.
+ */
+public class PoseClassifierProcessor {
+  private static final String TAG = "PoseClassifierProcessor";
+  private static final String POSE_SAMPLES_FILE = "pose/fitness_pose_samples.csv";
+
+  // Specify classes for which we want rep counting.
+  private static final String PUSHUPS_CLASS = "pushups_down";
+  private static final String SQUATS_CLASS = "squats_down";
+  private static final String[] POSE_CLASSES = {
+          PUSHUPS_CLASS, SQUATS_CLASS
+  };
+
+  private final boolean isStreamMode;
+
+  private EMASmoothing emaSmoothing;
+  private List<RepetitionCounter> repCounters;
+  private PoseClassifier poseClassifier;
+  private String lastRepResult;
+
+  public PoseClassifierProcessor(Context context, boolean isStreamMode) {
+    Log.d(TAG, "PoseClassifierProcessor constructor started.");
+    this.isStreamMode = isStreamMode;
+    if (isStreamMode) {
+      emaSmoothing = new EMASmoothing();
+      repCounters = new ArrayList<>();
+      lastRepResult = "";
+    }
+    loadPoseSamplesAsync(context);
+  }
+
+  private void loadPoseSamplesAsync(final Context context) {
+    new Thread(() -> loadPoseSamples(context)).start();
+  }
+
+  @WorkerThread
+  private void loadPoseSamples(Context context) {
+    List<PoseSample> poseSamples = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(context.getAssets().open(POSE_SAMPLES_FILE)))) {
+      String csvLine;
+      while ((csvLine = reader.readLine()) != null) {
+        PoseSample poseSample = PoseSample.getPoseSample(csvLine, ",");
+        if (poseSample != null) {
+          poseSamples.add(poseSample);
+        }
+      }
+      poseClassifier = new PoseClassifier(poseSamples);
+      if (isStreamMode) {
+        for (String className : POSE_CLASSES) {
+          repCounters.add(new RepetitionCounter(className));
+        }
+      }
+      Log.d(TAG, "Pose samples loaded successfully.");
+    } catch (IOException e) {
+      Log.e(TAG, "Error when loading pose samples.\n" + e);
+    }
+  }
+
+  /**
+   * Given a new {@link Pose} input, returns a list of formatted {@link String}s with Pose
+   * classification results.
+   *
+   * <p>Currently it returns up to 2 strings as follows:
+   * 0: PoseClass : X reps
+   * 1: PoseClass : [0.0-1.0] confidence
+   */
+  @WorkerThread
+  public List<String> getPoseResult(Pose pose) {
+    Log.d(TAG, "Getting pose result.");
+    if (Looper.myLooper() == Looper.getMainLooper()) {
+      throw new IllegalStateException("getPoseResult should not be called on the main thread.");
+    }
+    List<String> result = new ArrayList<>();
+    if (poseClassifier == null) {
+      Log.e(TAG, "PoseClassifier is not initialized.");
+      return result; // or handle as needed
+    }
+    ClassificationResult classification = poseClassifier.classify(pose);
+
+    if (isStreamMode) {
+      classification = emaSmoothing.getSmoothedResult(classification);
+
+      if (pose.getAllPoseLandmarks().isEmpty()) {
+        result.add(lastRepResult);
+        return result;
+      }
+
+      for (RepetitionCounter repCounter : repCounters) {
+        int repsBefore = repCounter.getNumRepeats();
+        int repsAfter = repCounter.addClassificationResult(classification);
+        if (repsAfter > repsBefore) {
+          ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+          tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+          lastRepResult = String.format(
+                  Locale.US, "%s : %d reps", repCounter.getClassName(), repsAfter);
+          break;
+        }
+      }
+      result.add(lastRepResult);
+    }
+
+    if (!pose.getAllPoseLandmarks().isEmpty()) {
+      String maxConfidenceClass = classification.getMaxConfidenceClass();
+      String maxConfidenceClassResult = String.format(
+              Locale.US,
+              "%s : %.2f confidence",
+              maxConfidenceClass,
+              classification.getClassConfidence(maxConfidenceClass)
+                      / poseClassifier.confidenceRange());
+      result.add(maxConfidenceClassResult);
+    }
+
+    return result;
+  }
+
+  public void cleanup() {
+    if (emaSmoothing != null) {
+      emaSmoothing.cleanup(); // Assuming emaSmoothing has a cleanup method
+    }
+    // Add other cleanup operations if needed
+    Log.d(TAG, "PoseClassifierProcessor cleaned up.");
+  }
+}
---
+++ app/src/main/java/com/example/smartposture/view/PoseDetectorActivity.java
@@ -0,0 +1,208 @@
+package com.example.smartposture.view;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.OptIn;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ExperimentalGetImage;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.example.smartposture.R;
+import com.example.smartposture.posedetector.GraphicOverlay;
+import com.example.smartposture.posedetector.classification.PoseClassifierProcessor;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.PoseDetector;
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class PoseDetectorActivity extends AppCompatActivity {
+    private static final String TAG = "PoseDetectorActivity";
+    private static final int PERMISSION_REQUESTS = 1;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
+    private GraphicOverlay graphicOverlay;
+    private PoseDetector poseDetector;
+    private PoseClassifierProcessor poseClassifierProcessor;
+    private HandlerThread handlerThread;
+    private Handler handler;
+    private final boolean runClassification = true;
+
+    @OptIn(markerClass = ExperimentalGetImage.class)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_push_up);
+
+        previewView = findViewById(R.id.previewView);
+        graphicOverlay = findViewById(R.id.graphicOverlay);
+
+        // Setup Pose Detector
+        PoseDetectorOptions options = new PoseDetectorOptions.Builder()
+                .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+                .build();
+        poseDetector = PoseDetection.getClient(options);
+
+        // Initialize HandlerThread for background processing
+        handlerThread = new HandlerThread("PoseClassifierProcessorThread");
+        handlerThread.start();
+        handler = new Handler(handlerThread.getLooper());
+
+        poseClassifierProcessor = new PoseClassifierProcessor(getApplicationContext(), true);
+
+        // Request permissions if not granted
+        if (!allPermissionsGranted()) {
+            getRuntimePermissions();
+        } else {
+            startCamera();
+        }
+    }
+
+    private void startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(TAG, "Error starting camera", e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(ActivityCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy imageProxy) {
+                ByteBuffer byteBuffer = imageProxy.getImage().getPlanes()[0].getBuffer();
+                byteBuffer.rewind();
+                Bitmap bitmap = Bitmap.createBitmap(imageProxy.getWidth(), imageProxy.getHeight(), Bitmap.Config.ARGB_8888);
+                bitmap.copyPixelsFromBuffer(byteBuffer);
+
+                if (bitmap != null) {
+                    InputImage image = InputImage.fromBitmap(bitmap, imageProxy.getImageInfo().getRotationDegrees());
+                    Task<Pose> result = poseDetector.process(image)
+                            .addOnSuccessListener(new OnSuccessListener<Pose>() {
+                                @Override
+                                public void onSuccess(Pose pose) {
+                                    List<String> classificationResult = new ArrayList<>();
+                                    if (poseClassifierProcessor != null && runClassification) {
+                                        handler.post(() -> {
+                                            classificationResult.addAll(poseClassifierProcessor.getPoseResult(pose));
+                                            runOnUiThread(() -> {
+                                                graphicOverlay.setImageSourceInfo(imageProxy.getHeight(), imageProxy.getWidth(), true);
+                                                graphicOverlay.clear();
+                                                graphicOverlay.add(new Display(graphicOverlay, pose, true, true, true, classificationResult));
+                                            });
+                                        });
+                                    }
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.e(TAG, "Pose detection failed", e);
+                                }
+                            });
+                }
+                imageProxy.close();
+            }
+        });
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageAnalysis, preview);
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void getRuntimePermissions() {
+        List<String> allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (poseClassifierProcessor != null) {
+            poseClassifierProcessor.cleanup();
+        }
+        if (handlerThread != null) {
+            handlerThread.quitSafely();
+        }
+    }
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info = this.getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            return info.requestedPermissions != null ? info.requestedPermissions : new String[0];
+        } catch (Exception e) {
+            Log.e(TAG, "Error getting required permissions", e);
+            return new String[0];
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
