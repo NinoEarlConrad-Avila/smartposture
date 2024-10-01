7f74297cf623c77b38148636903a2666b9051679
mikeronald-fuentes <myouimina10232002@gmail.com>
2024-10-01 14:25:00 +0800

    Add Notification Page UI
    successfully added notification fragment, no backend

--- app/src/main/java/com/example/smartposture/view/HomeFragment.java
+++ app/src/main/java/com/example/smartposture/view/HomeFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.smartposture.R;
 
@@ -16,6 +18,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView notification = view.findViewById(R.id.notification);
+
+        notification.setOnClickListener(v -> {
+            NotificationFragment notif = new NotificationFragment();
+
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, notif)
+                    .addToBackStack("Notification")
+                    .commit();
+        });
         return view;
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/smartposture/view/NotificationFragment.java
@@ -0,0 +1,22 @@
+package com.example.smartposture.view;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.smartposture.R;
+
+public class NotificationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+        return view;
+    }
+}
