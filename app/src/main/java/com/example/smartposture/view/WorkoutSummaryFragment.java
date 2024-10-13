eb7bd235f12b10577a3afc670c966b4ae7c387e4
mikeronald-fuentes <myouimina10232002@gmail.com>
2024-10-14 01:43:15 +0800

    Modify Workout Sumamry Fragment
    add description, modify goals UI, modify graph

--- app/src/main/java/com/example/smartposture/view/WorkoutSummaryFragment.java
+++ app/src/main/java/com/example/smartposture/view/WorkoutSummaryFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -33,43 +35,106 @@ public class WorkoutSummaryFragment extends Fragment {
 
     private void setGraphViewData(View view) {
         // Get the GraphView from the layout
-        GraphView graphView =(GraphView) view.findViewById(R.id.idGraphView);
+        GraphView graphView = (GraphView) view.findViewById(R.id.idGraphView);
 
-        // Create a new series with DataPoints for the graph
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(1, 0),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 1),
-                new DataPoint(5, 8),
-                new DataPoint(6, 10),
-                new DataPoint(7, 1),
-                new DataPoint(8, 2),
-                new DataPoint(9, 2),
-                new DataPoint(10, 5),
-
+        // Create two series (lines)
+        LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 0),
+                new DataPoint(1, 40),
+                new DataPoint(2, 70),
+                new DataPoint(3, 30),
+                new DataPoint(4, 30),
+                new DataPoint(5, 30),
         });
 
-        // Add the series to the graph
-        graphView.addSeries(series);
+//        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[]{
+//                new DataPoint(1, 5),
+//                new DataPoint(2, 6),
+//                new DataPoint(3, 7),
+//        });
 
-        // Customize the graph appearance
-        series.setColor(getResources().getColor(R.color.teal));
-        series.setDrawDataPoints(true);
-        series.setDataPointsRadius(1);
-        series.setThickness(3);
-        graphView.getGridLabelRenderer().setVerticalAxisTitle("Y Axis");
-        graphView.getGridLabelRenderer().setHorizontalAxisTitle("X Axis");
+        // Add both series to the graph
+        graphView.addSeries(series1);
+//        graphView.addSeries(series2);
 
-        // Enable scrolling and scaling
+        // Customize the appearance of both series
+        series1.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+        series1.setDrawDataPoints(true);
+        series1.setDataPointsRadius(1);
+        series1.setThickness(8);
+        series1.setColor(Color.BLUE);
+
+
+//        series2.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+//        series2.setDrawDataPoints(true);
+//        series2.setDataPointsRadius(1);
+//        series2.setThickness(3);
+
+        // Get the CheckBoxes from the layout
+//        CheckBox checkBox1 = view.findViewById(R.id.checkBox2);
+//        CheckBox checkBox2 = view.findViewById(R.id.checkBox1);
+//
+//        checkBox1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//            if (isChecked) {
+//
+//                checkBox2.setChecked(false);
+//
+//                series1.setThickness(8);
+//                series1.setColor(Color.RED);
+//
+//                series2.setThickness(3);
+//                series2.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+//            } else {
+//                if (!checkBox2.isChecked()) {
+//                    series1.setThickness(3);
+//                    series1.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+//                    series2.setThickness(3);
+//                    series2.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+//                }
+//            }
+//            graphView.invalidate();
+//        });
+//
+//        checkBox2.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//            if (isChecked) {
+//                checkBox1.setChecked(false);
+//
+//                series2.setThickness(8);
+//                series2.setColor(Color.GREEN);
+//
+//                series1.setThickness(3);
+//                series1.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+//            } else {
+//                if (!checkBox1.isChecked()) {
+//                    series1.setThickness(3);
+//                    series1.setColor(ContextCompat.getColor(getContext(), R.color.teal));
+//                    series2.setThickness(3);
+//                    series2.setColor(Color.RED);
+//                }
+//            }
+//            graphView.invalidate();
+//        });
+
+        graphView.getGridLabelRenderer().setNumVerticalLabels(5);   // Set number of labels on Y axis
+        graphView.getGridLabelRenderer().setNumHorizontalLabels(6);
+// Set manual bounds for the viewport (to control the min and max values on X and Y axes)
+        graphView.getViewport().setYAxisBoundsManual(true);
+        graphView.getViewport().setMinY(0);   // Set minimum Y value
+        graphView.getViewport().setMaxY(100);  // Set maximum Y value
+
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMinX(0);   // Set minimum X value
+        graphView.getViewport().setMaxX(5);  // Set maximum X value
+
+        graphView.getGridLabelRenderer().setVerticalAxisTitle("Angle");
+        graphView.getGridLabelRenderer().setVerticalAxisTitleColor(ContextCompat.getColor(getContext(), R.color.teal));
+        graphView.getGridLabelRenderer().setHorizontalAxisTitle("Repetition");
+        graphView.getGridLabelRenderer().setHorizontalAxisTitleColor(ContextCompat.getColor(getContext(), R.color.teal));
+        graphView.getGridLabelRenderer().setGridColor(ContextCompat.getColor(getContext(), R.color.teal));
+        graphView.getGridLabelRenderer().setHorizontalLabelsColor(ContextCompat.getColor(getContext(), R.color.teal));
+        graphView.getGridLabelRenderer().setVerticalLabelsColor(ContextCompat.getColor(getContext(), R.color.teal));
         graphView.getViewport().setScalable(true);
         graphView.getViewport().setScrollable(true);
-        graphView.getGridLabelRenderer().setGridColor(Color.RED); // Set both horizontal and vertical grid color
-        // Or individually set horizontal/vertical grid color
-        graphView.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLUE); // Horizontal grid color
-        graphView.getGridLabelRenderer().setVerticalLabelsColor(Color.GREEN);   // Vertical grid color
-        graphView.getGridLabelRenderer().setPadding(20);
-        // Additional customizations
-        graphView.getGridLabelRenderer().setHighlightZeroLines(true);
     }
+
 }
